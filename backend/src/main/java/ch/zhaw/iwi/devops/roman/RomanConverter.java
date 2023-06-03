f048c229e982d3369d25abbb045a139277d83dcf
Kisanth Ketheeswaran <kethekis@students.zhaw.ch>
2023-06-03 18:03:31 +0200

    Adding the successful code for Test 5

--- backend/src/main/java/ch/zhaw/iwi/devops/roman/RomanConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/roman/RomanConverter.java
@@ -11,6 +11,12 @@ public class RomanConverter {
             return "XL";
         else if (i == 10)
             return "X";
+        else if (i == 8)
+            return "VIII";
+        else if (i == 7)
+            return "VII";
+        else if (i == 6)
+            return "VI";
         else if (i == 5)
             return "V";
         else if (i == 4)
