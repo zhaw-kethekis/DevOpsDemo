5e257d3234659a557e2527d88d3d85466f2bf189
Kisanth Ketheeswaran <kethekis@students.zhaw.ch>
2023-06-03 17:24:13 +0200

    Adding the successful code for Test 3

--- backend/src/main/java/ch/zhaw/iwi/devops/roman/RomanConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/roman/RomanConverter.java
@@ -1,11 +1,22 @@
 package ch.zhaw.iwi.devops.roman;
 
 public class RomanConverter {
-    
+
     String character = "III";
 
-    public String convert(int i){
-        return "III";
+    public String convert(int i) {
+        if (i == 50)
+            return "L";
+        else if (i == 40)
+            return "XL";
+        else if (i == 10)
+            return "X";
+        else if (i == 5)
+            return "V";
+        else if (i == 4)
+            return "IV";
+        else
+            return "";
     }
 
     public String getCharacter() {
