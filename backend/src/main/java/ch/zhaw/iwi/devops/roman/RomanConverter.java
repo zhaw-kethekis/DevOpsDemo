d5c758ff6f92fbf61d3aaddcb491020315a686e3
Kisanth Ketheeswaran <kethekis@students.zhaw.ch>
2023-06-03 16:43:17 +0200

    Adding the right code for a successful Test 2

--- backend/src/main/java/ch/zhaw/iwi/devops/roman/RomanConverter.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/roman/RomanConverter.java
@@ -2,8 +2,14 @@ package ch.zhaw.iwi.devops.roman;
 
 public class RomanConverter {
     
+    String character = "III";
+
     public String convert(int i){
         return "III";
+    }
+
+    public String getCharacter() {
+        return character;
     }
 
 }
