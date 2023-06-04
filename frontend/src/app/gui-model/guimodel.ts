9dfa068abe5cb44b8b1adbd6ca51cbe820d91d3b
Kisanth Ketheeswaran <kethekis@students.zhaw.ch>
2023-06-04 17:32:28 +0200

    align for exercise Week 12

--- frontend/src/app/gui-model/guimodel.ts
+++ frontend/src/app/gui-model/guimodel.ts
@@ -10,7 +10,7 @@ export class GuiModel {
                     "title": "NotImplemented",
                     "formFieldList": [
                         {
-                            "type": 'okButton',
+                            "type": "okButton",
                             "name": "Ok"
                         }
                     ]
